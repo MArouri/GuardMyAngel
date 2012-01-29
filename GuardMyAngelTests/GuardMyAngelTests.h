25beb962b9d8ba554303a2a992c799ff9f57bef5
Mohammad Arouri <mohammadarouri@Mohammad-Arouris-Mac-mini.local>
2012-01-29 12:00:31 +0200

    Initial Commit

---
+++ GuardMyAngelTests/GuardMyAngelTests.h
@@ -0,0 +1,13 @@
+//
+//  GuardMyAngelTests.h
+//  GuardMyAngelTests
+//
+//  Created by Mohammad Arouri on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GuardMyAngelTests : SenTestCase
+
+@end
---
+++ GuardMyAngelTests/GuardMyAngelTests.m
@@ -0,0 +1,32 @@
+//
+//  GuardMyAngelTests.m
+//  GuardMyAngelTests
+//
+//  Created by Mohammad Arouri on 1/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "GuardMyAngelTests.h"
+
+@implementation GuardMyAngelTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GuardMyAngelTests");
+}
+
+@end
